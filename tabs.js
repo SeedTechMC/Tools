596078208b68ed23b34271e771a0ba957bff0ab0
Seed <admin@Seed.soy>
2025-03-29 21:19:50 +0000

    Create tabs.js

---
+++ tabs.js
@@ -0,0 +1,14 @@
+const linkData = {
+    buttons: [
+        { text: 'Page 1', href: '/page1' },
+        { text: 'Page 2', href: '/page2' },
+        { text: 'Page 3', href: '/page3' }
+    ],
+    json: [
+        { text: 'JSON', href: '/JSON' },
+        { text: 'Other Links', children: [
+            { text: 'Link 1', href: '/link1' },
+            { text: 'Link 2', href: '/link2' }
+        ]}
+    ]
+};
